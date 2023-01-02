c7e23f12f0233cc9c55aef28886dd6ea8653357c
Dejsakda <dejsakda.p@gmail.com>
2023-01-02 20:17:15 +0700

    add echo and set middleware

--- server.go
+++ server.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"log"
+	"net/http"
+	"os"
 
 	"github.com/joho/godotenv"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
@@ -12,4 +16,22 @@ func main() {
 	if err != nil {
 		log.Fatalf("Some error occured. Err: %s", err)
 	}
+
+	e := echo.New()
+
+	e.Use(middleware.KeyAuthWithConfig(middleware.KeyAuthConfig{
+		AuthScheme: "November",
+		Validator: func(key string, c echo.Context) (bool, error) {
+			return key == "10, 2009", nil
+		},
+	}))
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	e.GET("/expenses", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+
+	e.Logger.Fatal(e.Start(os.Getenv("PORT")))
 }
